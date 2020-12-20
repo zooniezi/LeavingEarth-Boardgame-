ff052cd760db986d2b402019d45377142338d647
zooniezi <zoonie04@khu.ac.kr>
2020-12-20 14:54:08 +0000

    button action complete

---
+++ main2.js
@@ -0,0 +1,28 @@
+//DATA
+
+//
+
+function accessById(id_code) {
+    return document.getElementById(id_code)
+}
+
+function accessByClass(class_code) {
+	return document.getElementsByClassName(class_code)
+}
+
+function boost(thrust, mass, difficulty) {
+	if(mass * difficulty <= thrust) {
+        return true
+    }
+    return false
+}
+function zooming(address){
+	var target = accessById("zoom")
+    var temp = accessByClass("mapimage")[address]
+	target.src = temp.src
+    alert(1) //디버그용
+}
+function popUp(target) {
+    var temp = accessById("area")
+    temp.src = "Interface/" + target + ".html"
+}
