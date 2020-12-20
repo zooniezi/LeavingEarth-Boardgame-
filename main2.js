5670533136798eefb0f26b05537c1b8617854eb5
zooniezi <zoonie04@khu.ac.kr>
2020-12-20 14:57:35 +0000

    button action completed

--- main2.js
+++ main2.js
@@ -16,12 +16,13 @@ function boost(thrust, mass, difficulty) {
     }
     return false
 }
+/*TODO function
 function zooming(address){
 	var target = accessById("zoom")
     var temp = accessByClass("mapimage")[address]
 	target.src = temp.src
     alert(1) //디버그용
-}
+}*/
 function popUp(target) {
     var temp = accessById("area")
     temp.src = "Interface/" + target + ".html"
